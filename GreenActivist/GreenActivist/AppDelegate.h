24042b8bca768bc50dcd59fd0c73b1ae9f558f31
Miguel <mureatencio@gmail.com>
2013-09-09 17:42:39 -0600

    Creacion del proyecto con Parse.

---
+++ GreenActivist/GreenActivist/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GreenActivist
+//
+//  Created by GoTouch on 09/09/13.
+//  Copyright (c) 2013 GoTouch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GreenActivist/GreenActivist/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  GreenActivist
+//
+//  Created by GoTouch on 09/09/13.
+//  Copyright (c) 2013 GoTouch. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
+    [testObject setObject:@"bar" forKey:@"foo"];
+    [testObject save];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
