2eb4577d324966078a2aa2ea1aa24ae23382b9ac
lumurillo <usuario@MacBook-Pro-de-administrador.local>
2013-09-10 03:42:25 -0600

    Fondos de todos los Views

---
+++ GreenActivist/GreenActivist/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController {
+    
+}
+
+//@property (nonatomic, strong) IBOutlet UIActivityIndicatorView *activityIndicator;
+
+- (IBAction)loginButtonTouchHandler:(id)sender;
+
+@end
